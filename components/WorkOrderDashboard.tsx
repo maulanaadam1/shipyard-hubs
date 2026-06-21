64c07c1c9c42c45c84b71222c65ad53361b0a123
maulanaadam1 <115075744+maulanaadam1@users.noreply.github.com>
2026-06-22 04:22:34 +0700

    style: fix WO filter layout overlap on tablet portrait

--- components/WorkOrderDashboard.tsx
+++ components/WorkOrderDashboard.tsx
@@ -1909,7 +1909,7 @@ export default function WorkOrderDashboard() {
             </button>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
             
             {/* Search Input */}
             <div className="relative">
